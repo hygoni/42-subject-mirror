858af9f35ecea2a4bc6ac2074db88eb9826513c2
hygoni <9hrack@naver.com>
2020-09-25 23:33:57 +0900

    [REFACTOR] reduce hard coding (os dependency)

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -3,16 +3,16 @@ import os
 import time
 import getpass
 
-# It is assumed that you run it on OS X
-# you should edit some paths to run on another environment
-
 # Constants
 LOGIN_URL = 'https://signin.intra.42.fr/users/sign_in'
 PROJECT_URL = 'https://projects.intra.42.fr/projects/list'
 USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'    
-DOWNLOAD_PATH = os.path.expanduser('~/Downloads')
+DOWNLOAD_PATH = os.getcwd()
 # Load Chrome Driver
-prefs = {"plugins.always_open_pdf_externally": True, "plugins.plugins_disabled": ["Chrome PDF Viewer"]}
+prefs = {"plugins.always_open_pdf_externally": True, 
+    "plugins.plugins_disabled": ["Chrome PDF Viewer"],
+    "download.default_directory": DOWNLOAD_PATH
+}
 options = webdriver.ChromeOptions()
 options.add_argument('user-agent={}'.format(USER_AGENT))
 options.add_experimental_option("prefs", prefs)
@@ -33,7 +33,6 @@ link_list = []
 for link in links:
     link = link.find_element_by_tag_name('a').get_attribute('href')
     link_list.append(link)
-os.system("rm ~/Downloads/en.subject*")
 
 # Iterate each link
 for link in link_list:
@@ -46,9 +45,8 @@ for link in link_list:
         while os.path.exists(os.path.join(DOWNLOAD_PATH, 'en.subject.pdf')) == False:
           print('Waiting for download...', project_name)
           time.sleep(1)
-        print(link)
         print(project_name)
-        os.system('mv ~/Downloads/en.subject.pdf ' + '~/goinfre/' + project_name)
+        os.system('mv {}/en.subject.pdf {}/{}.pdf'.format(DOWNLOAD_PATH, DOWNLOAD_PATH, project_name))
     except:
         pass
     time.sleep(1)
