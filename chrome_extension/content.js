87d8375829aa7205a78fa262de51294f6c9a2e34
hyeyoo <50040414+hygoni@users.noreply.github.com>
2020-09-28 16:53:55 +0900

    [FIX] bug fix when the page is someone's project page

--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -1,12 +1,24 @@
 window.onload = function() {
     let links = window.location.href.split('/');
-    const projectName = links[links.length - 1];
-    const elementString = `<div class="project-attachment-item">
+    let projectName;
+
+	/* if there's description, it's not someone's project page! */
+	let description = document.getElementsByClassName('project-desc-item')[0];
+	if (description == undefined) {
+		projectName = links[links.length - 2];
+	} else {
+		projectName = links[links.length - 1];
+	}
+
+	/* generate link HTML */
+	const elementString = `<div class="project-attachment-item">
 <h4 class="attachment-name">
 <span class="icon-file"></span>
 <a target="_blank" href="http://42subject.hygoni.com/subjects/${projectName}.pdf">mirror of subject.pdf</a>
 </h4>
 </div>`;
+
+	/* add link in parent (as first element) */
     const parentElement = document.getElementsByClassName("project-attachments-list")[0];
     if (parentElement == undefined)
         return ;
